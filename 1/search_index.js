9570f56dcdc29ab44d6084c4a2218e5d3cc4a43e
Documenter.jl <documenter@juliadocs.github.io>
2022-10-23 15:46:18 +0000

    build based on af36863

---
+++ 1/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = githubaction_example_for_julia","category":"page"},{"location":"#githubaction*example*for_julia","page":"Home","title":"githubactionexamplefor_julia","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for githubactionexamplefor_julia.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [githubaction_example_for_julia]","category":"page"}]
+}
